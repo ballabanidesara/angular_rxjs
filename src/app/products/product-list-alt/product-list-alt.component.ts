49a3f8153cfbc9cad82a4c8dbef6d89abac6b0a0
ballabanidesara <desara.ballabani@base-com.al>
2022-05-05 16:10:03 +0200

    Reacting to an error

--- src/app/products/product-list-alt/product-list-alt.component.ts
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { catchError, EMPTY, Subscription } from 'rxjs';
+import { catchError, EMPTY, Subject, Subscription } from 'rxjs';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -13,12 +13,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
-  errorMessage = '';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
 
   products$ = this.productService.productsWithCategory$
   .pipe(
     catchError(err => {
-      this.errorMessage = err;
+      this.errorMessageSubject.next(err);
       return EMPTY;
     })
    );
